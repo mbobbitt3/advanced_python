7d4f1df65a3bb3ab23f302b1cd7c23b4ee6aa31c
matt <mattbobitt3@gmail.com>
2024-04-25 17:20:48 -0400

    graph looks semi decent still need to refactor ghidra_cfg.py to be more object oriented, but it's good for now

--- final_project/scripts/ghidra_cfg.py
+++ final_project/scripts/ghidra_cfg.py
@@ -1,4 +1,3 @@
-from ghidra.util.graph import DirectedGraph
 from ghidra.util.graph import Edge
 from ghidra.util.graph import Vertex
 from ghidra.util.task import * 
@@ -23,13 +22,13 @@ class CFG:
 
 	def func_conv(self, offset):
 		'''this method converts a raw hex address input into a Ghidra address object
-		and then gets the function object contianing that address''' 
+		and then gets the function object from  that address''' 
 		f_addr = self.prog.getAddressFactory().getDefaultAddressSpace().getAddress(offset)			
 		f = self.prog.getFunctionManager().getFunctionContaining(f_addr)
 		return f
 
 	def gen_cfg_dict(self):
-		'''get all basic blocks and callee basic blocks within function and store as pairs representing (caller, callee)
+		'''get all basic blocks and respective callee blocks within function and store as pairs representing (caller, callee)
 		into a dictionary that we will use to make the graph'''
 		monitor = ConsoleTaskMonitor()
 		model = SimpleBlockModel(self.prog) 
@@ -37,29 +36,37 @@ class CFG:
 		f_name  = self._func.getName()
 		edge_map= OrderedDict()
 		edge_map[f_name] = []
+		bb = block_iter.next()
+		entry_node = False 
 		while block_iter.hasNext():
-			bb = block_iter.next()
-			bb_name = bb.getName()
-			bb_name = bb_name.strip('LAB_')
-		#	block['instructions'] = self.prog.getListing().getInstructions(bb,True) 
 			bb_dests = bb.getDestinations(monitor)
-			bb_succ = bb_dests.next()
 			while bb_dests.hasNext():
-				bb_succ_name = str(bb_succ).split('> ')[-1]
-				edge_map[f_name].append((bb_name, bb_succ_name))
+				bb_name = bb.getName()
+				bb_name = bb_name.strip('LAB_')
 				bb_succ = bb_dests.next()
+				bb_succ_name = str(bb_succ).split('> ')[-1]
+				edge_map[f_name].append((bb_name, bb_succ_name, bb.getFlowType().toString()))
+				
+				if not entry_node:
+					entry_node = True
+					edge_map['entry_node'] = bb_name
+				
+				edge_map['exit_node'] = bb_succ_name
+				
+				print(bb_name, bb_succ_name)
 
+			bb = block_iter.next()
 
 		return edge_map
 
-	def save_cfg(self):
+	def save_cfg(self, cfg_dict):
 		'''conjoin elements witin kwords to make file name and then write to json output for graphing'''
 		kwords = [self.prog.getName().replace('.','_'), self._func.getName()]
 		out_name = '_'.join(kwords)
-		cfg_dict = self.gen_cfg_dict()
 		print(cfg_dict)
-		with open('cfg_json_files/' + out_name + '.json', 'w+') as fd:
+		with open('../cfg_json_files/' + out_name + '.json', 'w+') as fd:
 			json.dump(cfg_dict, fd)
-
+		
 cfg = CFG(0x0010146b)
-cfg.save_cfg()
+cfg_dict = cfg.gen_cfg_dict()
+cfg.save_cfg(cfg_dict)
--- final_project/scripts/graph_generation.py
+++ final_project/scripts/graph_generation.py
@@ -7,19 +7,60 @@ class Cfg_Graph:
 		with open("../cfg_json_files/sample_out_main.json", 'r') as fd:
 			self.dict = json.load(fd)
 		self.cfg = self.dict['main']
-		self.graph = nx.DiGraph(self.cfg)
+		self.graph = nx.DiGraph()
+		self.flow_types = [
+			"CALL_OVERRIDE_UNCONDITIONAL",
+			"CALL_TERMINATOR",
+			"CALLOTHER_OVERRIDE_CALL",
+			"CALLOTHER_OVERRIDE_JUMP",
+			"COMPUTED_CALL",
+			"COMPUTED_CALL_TERMINATOR",
+			"COMPUTED_JUMP",
+			"CONDITIONAL_CALL",
+			"CONDITIONAL_CALL_TERMINATOR",
+			"CONDITIONAL_COMPUTED_CALL",
+			"CONDITIONAL_COMPUTED_JUMP",
+			"CONDITIONAL_JUMP",
+			"CONDITIONAL_TERMINATOR",
+			"DATA",
+			"DATA_IND",
+			"EXTERNAL_REF",
+			"FALL_THROUGH",
+			"FLOW",
+			"INDIRECTION",
+			"INVALID",
+			"JUMP_OVERRIDE_UNCONDITIONAL",
+			"JUMP_TERMINATOR",
+			"PARAM",
+			"READ",
+			"READ_IND",
+			"READ_WRITE",
+			"READ_WRITE_IND",
+			"TERMINATOR",
+			"THUNK",
+			"UNCONDITIONAL_CALL",
+			"UNCONDITIONAL_JUMP",
+			"WRITE",
+			"WRITE_IND"
+	]
 
-	def create_graph(self, flow_dict):
+	def create_graph(self):
 		# Loop through each key-value pair in the dictionary
-		for source, targets in self.cfg.items():
-			for target in targets:
-				self.graph.add_edge(source, target)  # Add edge from source to target
-
-	def visualize_graph(self):
+		#for node in self.cfg:
+		#	print(node)
+		for source, target, flow_type in self.cfg:
+			print(source, target, flow_type)
+			self.graph.add_edge(source, target, label= flow_type)  # Add edge from source to target
+		return self.graph
+	
+	def visualize_graph(self, graph):
 		# Generate the visualization
-		plt.figure(figsize=(10, 6))
-		pos = nx.spring_layout(self.graph)	# Positions for all nodes
-		nx.draw(self.graph, pos, with_labels=True, node_size=700, node_color='lightblue', edge_color='gray', font_size=10)
+		plt.figure(figsize=(100, 60))
+		pos = nx.nx_agraph.graphviz_layout(self.graph, prog='dot')	# Positions for all nodes
+		nx.draw(self.graph, pos, with_labels=True, node_size=700, node_color='lightblue', edge_color='gray', font_size=7)
+		edge_labels = nx.get_edge_attributes(self.graph, 'label')
+		nx.draw_networkx_edge_labels(self.graph, pos, edge_labels = edge_labels, font_size = 8, label_pos = 0.5, rotate=
+		False)
 		plt.title("Control Flow Graph")
 		plt.show()	# Display the graph
 	
@@ -31,5 +72,5 @@ class Cfg_Graph:
 		plt.show()
 	'''
 cfg = Cfg_Graph()
-cfg.visualize_graph()
-
+cfg_obj = cfg.create_graph()
+cfg.visualize_graph(cfg_obj)
